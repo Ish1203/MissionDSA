9116f280cd9cab0955cdfab7de07b85079b075eb
Ish Dubey <ish.dubey1203@gmail.com>
2025-05-12 16:04:38 +0530

    Questions

--- MissionDSA/hackerrank/acmteam.py
+++ MissionDSA/hackerrank/acmteam.py
@@ -1,4 +1,5 @@
 def acmTeam(topic):
+    #Main function to find the maximum number of topics known by a team
     teams = list(itertools.combinations(topic, 2))
     subjects = []
     for team in teams:
@@ -6,5 +7,5 @@ def acmTeam(topic):
         for topic in range(len(team[0])):
             if team[0][topic] == "1" or team[1][topic] == "1":
                 known += 1
-        subjects.append(known)
+        subjects.append(known)  
     return [max(subjects), subjects.count(max(subjects))]
